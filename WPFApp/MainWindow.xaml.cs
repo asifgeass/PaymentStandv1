f047dab4e789f204b5f421575e3020923c223586
1as1 (work) <asifgeass@gmail.com>
2019-09-23 18:19:54 +0300

    Add project files.

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Clear();
+            Assembly[] sborki = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (Assembly item in sborki)
+            {
+                listBox1.Items.Add(item.FullName);
+            }            
+        }
+    }
+}
