f1356b9d79dc43ea9e33cef344623f5326332e75
1as1 (work) <asifgeass@gmail.com>
2019-11-11 22:42:59 +0300

    added конвертирование и суммирование валюты для передачи на терминал POS

--- Logic/POSManager.cs
+++ Logic/POSManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExceptionManager;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,15 +37,39 @@ namespace Logic
         }
         public MDOM_POS PayPURRequest(PayRecord payrecArg)
         {
-            Request.EnumType = PosQAType.PURRequest;
-            Request.ResponseReq.PaySumma = payrecArg.Summa;
-            //TEST
-            if (Request.ResponseReq.PaySumma == "0.00")
+            if (payrecArg == null) { Ex.Throw<ArgumentNullException>($"{nameof(PayPURRequest)}(): argument {nameof(PayRecord)}=null"); }
+            if (string.IsNullOrEmpty(payrecArg.Summa)) { Ex.Throw($"{nameof(PayPURRequest)}(): argument PayRecord.Summa is null or empty"); }
+            decimal summa = 0;
+            decimal fine = 0;
+            decimal commission = 0;
+            Ex.Throw($"{nameof(PayPURRequest)}(): не удалось конвертировать PayRecord.Summa='{payrecArg.Summa}' в цифровое значение"
+                , () => summa = StringToDecimal(payrecArg.Summa));
+            if (payrecArg.Fine != null)
             {
-                Request.ResponseReq.PaySumma = "1";
+                Ex.Throw($"{nameof(PayPURRequest)}(): не удалось конвертировать PayRecord.Fine='{payrecArg.Summa}' в цифровое значение"
+                    , () => fine = StringToDecimal(payrecArg.Fine));
+            }
+            if (payrecArg.PayCommission != null)
+            {
+                Ex.Throw($"{nameof(PayPURRequest)}(): не удалось конвертировать PayRecord.PayCommission='{payrecArg.Summa}' в цифровое значение"
+                  , () => commission = StringToDecimal(payrecArg.PayCommission));
+            }
+            decimal PayAllPOS = summa + fine + commission;
+            //TEST
+            if (PayAllPOS == 0)
+            {
+                PayAllPOS = 1.11M;
             }
             //TEST
+            Request.ResponseReq.PaySumma = PayAllPOS.ToString().Replace(',','.');
+            Request.EnumType = PosQAType.PURRequest;
+
             return Request;
+        }
+
+        private static decimal StringToDecimal(string argString)
+        {
+            return decimal.Parse(argString.Replace(',', '.'), NumberStyles.Currency, CultureInfo.InvariantCulture);
         }
 
         public MDOM_POS GetCancelVOIRequest(PS_ERIP reqArg)
